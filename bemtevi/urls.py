740fb50c29ef9e064d7dd3a1cb86933c6c14f0fe
Gabriel Gontijo <gontijogabr@gmail.com>
2023-11-09 12:00:23 -0300

    feat: bemtevi.urls

--- bemtevi/urls.py
+++ bemtevi/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from bemtevi.views import login
+from bemtevi.views import login, login_senha
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('loginsenha/', login_senha, name='login-senha'),
 ]
--- bemtevi/views.py
+++ bemtevi/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'login.html')
+
+def login_senha(request):
+    return render(request, 'login_senha.html')
