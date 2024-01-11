e8f751bb47895b7cb609e18c76c475d1ca1ca16c
gontijogabriel <gabriel.melo@defensoria.df.gov.br>
2024-01-11 12:19:21 -0300

    att html

--- bemtevi/models.py
+++ bemtevi/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
 
 class Tweet(models.Model):
     class Meta:
