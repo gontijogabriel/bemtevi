9dc5753d1c9f51a9d0a45fb6017f3f19a084c8f7
Gabriel Gontijo <gontijogabr@gmail.com>
2024-01-14 22:36:08 -0300

    media nos templates ok

---
+++ static/scripts/home.js
@@ -0,0 +1,17 @@
+function openModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+}
+
+// Fechar o modal se clicar fora da área do modal
+window.onclick = function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
